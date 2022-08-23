48348a53b19d98121e9f1572f841996b2c2d3de3
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-08-23 13:25:57 +0200

    Backend

---
+++ ComputerHardwarecapstoneproject/Frontend/desktopgaming/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Products from '../views/Products.vue'
+import Product from '../views/Product.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: Products
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: Product
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
